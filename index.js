09213040264d45642b898df58fdc24358e6434f8
Celio Grand <celiogrand@outlook.com>
2022-11-17 15:20:38 +0800

    Screenshot

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const { app, BrowserWindow, screen } = require('electron')
+const { app, BrowserWindow, screen, desktopCapturer } = require('electron')
 const path = require('path')
+const fs = require('fs')
 
 
+let win
 function createWindow() {
     const factor = screen.getPrimaryDisplay().scaleFactor
 
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 104 / factor,
         height: 124 / factor,
         frame: false,
@@ -21,12 +23,13 @@ function createWindow() {
     win.loadFile('index.html')
 
     win.setAlwaysOnTop(true, 'screen')
-    //win.setFocusable(false)
+    // win.setFocusable(false)
     win.setFullScreenable(false)
     win.setHasShadow(false)
     win.setMinimizable(false)
+    win.setMinimumSize(104, 124)
 
-    //win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 }
 
 
@@ -45,4 +48,25 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
+})
+
+let count = 0
+app.whenReady().then(() => {
+    setInterval(() => {
+        desktopCapturer.getSources({
+            // Frameless window can't be detected by type 'window' as of now.
+            // Using the crop of full screenshot.
+            types: ['screen'], thumbnailSize: screen.getPrimaryDisplay().size
+        }).then(sources => {
+            for (const s of sources) {
+                const fw = BrowserWindow.getAllWindows()[0]
+                const img = s.thumbnail.crop(fw.getBounds())
+                fs.writeFile(`test${count}.png`, img.toPNG(), (err) => {
+                    if (err) throw err
+                    console.log('Image Saved')
+                    count++
+                })
+            }
+        })
+    }, 1000)
 })
